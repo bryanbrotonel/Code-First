4e364d8192df17e0cc061e3ce1f396a646450389
bryan <bryan@BRYANBROTONA0C4>
2018-01-31 14:46:17 -0800

    Changed to ApplicationDbContext

--- Code First Lab/Controllers/ProvincesController.cs
+++ Code First Lab/Controllers/ProvincesController.cs
@@ -6,13 +6,14 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Code_First_Lab.Models;
 using Code_First_Lab.Models.Places;
 
 namespace Code_First_Lab.Controllers
 {
     public class ProvincesController : Controller
     {
-        private PlacesContext db = new PlacesContext();
+        private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Provinces
         public ActionResult Index()
